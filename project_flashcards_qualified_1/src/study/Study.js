99057129822fdc18f81531bb2cafe4bab8b9635a
Carlos Blandino <cablandino66@gmail.com>
2021-03-16 20:19:58 -0700

    study component: working on insufficient card message

--- project_flashcards_qualified_1/src/study/Study.js
+++ project_flashcards_qualified_1/src/study/Study.js
@@ -10,6 +10,79 @@ export default function Study({ name }) {
   const [card, setCard] = useState({});
   const history = useHistory();
 
+  function notEnoughCards(length) {
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/" className="btn btn-link">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="18"
+                fill="currentColor"
+                class="bi bi-house-door"
+                viewBox="0 0 20 20"
+              >
+                <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
+              </svg>
+              Home
+            </Link>
+          </li>
+          <li
+            className="breadcrumb-item active"
+            aria-current="page"
+            style={{ marginTop: "7px" }}
+          >
+            {name}
+          </li>
+          <li
+            className="breadcrumb-item active"
+            aria-current="page"
+            style={{ marginTop: "7px" }}
+          >
+            Study
+          </li>
+        </ol>
+      </nav>
+      <div>
+        <h1>Study:{name}</h1>
+      </div>
+
+      {/* card section */}
+
+      <div className="card" style={{ marginTop: "10px", maxWidth: "800px" }}>
+        <div className="card-body">
+          <h6>Not enough cards.</h6>
+          <p>
+            You need at least 3 cards to study. There are {length} cards in this
+            deck.
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              {flip && (
+                <Link
+                  to="#"
+                  className="btn btn-primary"
+                  style={{ margin: "0 10px" }}
+                  onClick={handleNext}
+                >
+                  Next
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* end of card section*/}
+    </div>;
+  }
   useEffect(() => {
     const abortController = new AbortController();
     async function loadCards() {
@@ -18,6 +91,7 @@ export default function Study({ name }) {
           parseInt(deckId),
           abortController.signal
         );
+
         setCards(tempCards);
       } catch (error) {
         if (error.name === "AbortError") {
@@ -52,6 +126,7 @@ export default function Study({ name }) {
     reLoadCard();
     return () => abortController.abort();
   }, [cards]);
+
   function restartCancelStudy() {}
   function handleFlip() {
     setFlip(!flip);
@@ -80,9 +155,6 @@ export default function Study({ name }) {
         setCardNumber(1);
       }
     }
-
-    //
-    // }
   }
 
   async function handleNext() {
@@ -153,6 +225,7 @@ export default function Study({ name }) {
           >
             {name}
           </li>
+
           <li
             className="breadcrumb-item active"
             aria-current="page"
@@ -162,48 +235,95 @@ export default function Study({ name }) {
           </li>
         </ol>
       </nav>
-      <div>
-        <h1>Study:{name}</h1>
-      </div>
+      {cards.length < 3 ? (
+        <div>
+          <h1>{name}: Study</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Study:{name}</h1>
+        </div>
+      )}
 
-      {/* card section */}
-
-      <div className="card" style={{ marginTop: "10px", maxWidth: "800px" }}>
-        <div className="card-body">
-          <h6>
-            Card {cardNumber} of {cards.length}
-          </h6>
-          <p className="card-text">{flip === true ? card.back : card.front}</p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-            }}
-          >
-            <div>
-              <Link
-                to="#"
-                className="btn btn-secondary"
-                style={{ marginRight: "10px" }}
-                onClick={handleFlip}
-              >
-                Flip
-              </Link>
-              {flip && (
+      {cards.length < 3 ? (
+        <div className="card" style={{ marginTop: "10px", maxWidth: "800px" }}>
+          <div className="card-body">
+            <h3>Not enough cards.</h3>
+            <p>
+              You need at least 3 cards to study. There are {cards.length} cards
+              in this deck.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>
                 <Link
                   to="#"
                   className="btn btn-primary"
                   style={{ margin: "0 10px" }}
                   onClick={handleNext}
                 >
-                  Next
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    fill="currentColor"
+                    class="bi bi-plus"
+                    viewBox="2 1 16 16"
+                  >
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                  </svg>
+                  Add Cards
                 </Link>
-              )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="card" style={{ marginTop: "10px", maxWidth: "800px" }}>
+          <div className="card-body">
+            <h6>
+              Card {cardNumber} of {cards.length}
+            </h6>
+            <p className="card-text">
+              {flip === true ? card.back : card.front}
+            </p>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>
+                <Link
+                  to="#"
+                  className="btn btn-secondary"
+                  style={{ marginRight: "10px" }}
+                  onClick={handleFlip}
+                >
+                  Flip
+                </Link>
+                {flip && (
+                  <Link
+                    to="#"
+                    className="btn btn-primary"
+                    style={{ margin: "0 10px" }}
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       {/* end of card section*/}
     </div>
   );
